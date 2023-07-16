c1287a0bfd2fe9081c2bc8ebbc031d475b60b2fd
garryRocks <gauravj1402@gmail.com>
2023-07-16 17:31:09 +0530

    second commit

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:3000', {
+  const response = await fetch('https://codegpt-e2gm.onrender.com', {
     method: 'POST', 
     headers: {
       'Content-type': 'application/json'
